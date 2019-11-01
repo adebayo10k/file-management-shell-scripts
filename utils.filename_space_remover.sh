98c8b124a30a0b00bea43a7988d07025fcfb43d8
damola adebayo <adebayo10k@gmail.com>
2019-11-01 13:58:51 +0000

    completes functions to get and validate user input

--- utils.filename_space_remover.sh
+++ utils.filename_space_remover.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
-
-# linux doesn't like spaces in filenames
-# renames files in a directory, with space characters replaced by underscores
+#: Title		:utils.filename_space_remover.sh
+#: Date			:2019-10-26
+#: Author		:adebayo10k
+#: Version		:1.0
+#: Description	:linux doesn't like spaces in filenames
+#: Description	:renames files in a directory, with space characters replaced by underscores
+#: Description	:
+#: Options		:
+##
 
 function main()
 {
+    source_dir_fullpath="" # OR #test_line=""
     abs_filepath_regex='^(/{1}[A-Za-z0-9\._-~]+)+$' # absolute file path, ASSUMING NOT HIDDEN FILE, ...
     all_filepath_regex='^(/?[A-Za-z0-9\._-~]+)+$' # both relative and absolute file path
 
     get_dir_to_check
+
+    exit 0
+
     find_files_and_show_user
     rename_files #
 
 
 } ## end main
-
 
 
 ###########################################################
@@ -23,13 +32,15 @@ function main()
 function get_dir_to_check()
 {
     echo "Enter the full path to the directory containing the files of interest" && echo
-    read source_dir_fullpath 
+    read source_dir_fullpath
+
+    sanitise_absolute_path_value "$source_dir_fullpath"
 
     # this valid form test works for both sanitised directory paths and absolute file paths
     test_file_path_valid_form "$source_dir_fullpath"
     if [ $? -eq 0 ]
     then
-        echo "SYNCHRONISED LOCATION HOLDING (PARENT) DIRECTORY PATH IS OF VALID FORM"
+        echo "SOURCE DIRECTORY PATH IS OF VALID FORM"
     else
         echo "The valid form test FAILED and returned: $?"
         echo "Nothing to do now, but to exit..." && echo
@@ -37,8 +48,17 @@ function get_dir_to_check()
         #exit $E_UNEXPECTED_ARG_VALUE
     fi	
 
-
-    #test_dir_path_access...
+    # if the above test returns ok, ...
+	test_dir_path_access "$source_dir_fullpath"
+	if [ $? -eq 0 ]
+	then
+		echo "The full path to the SOURCE DIRECTORY is: $source_dir_fullpath"
+	else
+		echo "The SOURCE DIRECTORY path access test FAILED and returned: $?"
+		echo "Nothing to do now, but to exit..." && echo
+        exit 1
+		#exit $E_REQUIRED_FILE_NOT_FOUND
+	fi    #...
 }
 
 ###########################################################
@@ -110,7 +130,8 @@ function test_file_path_valid_form
 		echo "PARAMETER WAS NOT A MATCH FOR OUR KNOWN PATH FORM REGEX: "$abs_filepath_regex"" && sleep 1 && echo
 		echo "Returning with a non-zero test result..."
 		test_result=1
-		return $E_UNEXPECTED_ARG_VALUE
+        return 1
+		#return $E_UNEXPECTED_ARG_VALUE
 	fi 
 
 
@@ -119,6 +140,66 @@ function test_file_path_valid_form
 	return "$test_result"
 }
 
+###############################################################################################
+
+# FINAL OPERATION ON VALUE, SO GLOBAL test_line SET HERE. RENAME CONCEPTUALLY DIFFERENT test_line NAMESAKES
+function sanitise_absolute_path_value ##
+{
+
+echo && echo "Entered into function ${FUNCNAME[0]}" && echo
+
+	# sanitise values
+	# - trim leading and trailing space characters
+	# - trim trailing / for all paths
+	test_line="${1}"
+	echo "test line on entering "${FUNCNAME[0]}" is: $test_line" && echo
+
+	# TRIM TRAILING AND LEADING SPACES AND TABS
+	test_line=${test_line%%[[:blank:]]}
+	test_line=${test_line##[[:blank:]]}
+
+	# TRIM TRAILING / FOR ABSOLUTE PATHS:
+	test_line=${test_line%'/'}
+	#test_line=${test_line%\/}
+
+	echo "test line after trim cleanups in "${FUNCNAME[0]}" is: $test_line" && echo
+
+    source_dir_fullpath="$test_line"
+
+    echo "source_dir_fullpath after trim cleanups in "${FUNCNAME[0]}" is: $source_dir_fullpath" && echo
+
+echo && echo "Leaving from function ${FUNCNAME[0]}" && echo
+
+}
+###############################################################################################
+# need to test for access and write permission to the file holding directory
+# 
+function test_dir_path_access
+{
+	echo && echo "Entered into function ${FUNCNAME[0]}" && echo
+
+	test_result=
+	test_dir_fullpath=$1
+
+	echo "test_dir_fullpath is set to: $test_dir_fullpath"
+
+	if [ -d "$test_dir_fullpath" ] && cd "$test_dir_fullpath" 2>/dev/null && [ -w "$test_dir_fullpath" ]
+	then
+		# directory file found and accessible
+		echo "directory "$test_dir_fullpath" found and accessed ok" && echo
+		test_result=0
+	else
+		# -> return due to failure of any of the above tests:
+		test_result=1
+		echo "Returning from function \"${FUNCNAME[0]}\" with test result code: xxx-xx" #$E_REQUIRED_FILE_NOT_FOUND"
+		return 1 
+        #return $E_REQUIRED_FILE_NOT_FOUND
+	fi
+
+	echo && echo "Leaving from function ${FUNCNAME[0]}" && echo
+
+	return "$test_result"
+}
 ###############################################################################################
 
 main "$@"; exit
